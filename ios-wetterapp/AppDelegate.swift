48147916788d8b852ab833a6537941cfe1cce9f7
Silas Schaprian <silas.schaprian@alumni.fh-aachen.de>
2024-02-15 16:08:09 +0100

    funktioniert jetzt irgendwie

--- ios-wetterapp/AppDelegate.swift
+++ ios-wetterapp/AppDelegate.swift
@@ -10,7 +10,7 @@ import BackgroundTasks
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -19,21 +19,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     
     func applicationDidEnterBackground(_ application: UIApplication) {
         NSLog("App did enter background")
@@ -44,9 +44,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     @available(iOS 13.0, *)
     func registerBackgroundTask(){
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "ios-wetterapp.fetchWeather", using: nil, launchHandler: {task in
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "ios-wetterapp.fetchWeather", using: nil) {task in
             self.handleRefreshTask(task as! BGAppRefreshTask)
-        })
+        }
         NSLog("Backgroundtask registriert!")
     }
     
@@ -61,11 +61,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Set the interval for how often the task should be performed (in seconds)
         request.earliestBeginDate = Date(timeIntervalSinceNow: 5) // 5 seconds
-    
+        
         do {
-            NSLog("schedule background task")
             // Schedule the background task
             try BGTaskScheduler.shared.submit(request)
+            NSLog("scheduled background task!")
         } catch {
             print("Error scheduling background task: \(error)")
         }
@@ -76,10 +76,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             NSLog("The Task expired!")
             task.setTaskCompleted(success: false)
         }
-        NSLog("task ran successfully")
+        
+        NSLog("Task completed successfully.")
         task.setTaskCompleted(success: true)
         scheduleRefreshBackgroundTask()
+        
     }
-
+    
 }
 
